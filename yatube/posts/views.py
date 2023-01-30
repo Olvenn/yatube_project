53bb4da4227b6303f2c3d6c5fb479dff16d5b8ae
Olvenn <olga-vencel@yandex.ru>
2023-01-30 14:33:41 +0300

    Create view functions for project pages: the main page and the posts page for a specific group.

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+# Главная страница
+def index(request):
+    return HttpResponse('Главная страница')
+
+
+# Страница со списком мороженого
+def group_posts(request, slug):
+    return HttpResponse(('Cтраница, на которой будут посты,'
+                         f'для группы под названием {slug}.'))
