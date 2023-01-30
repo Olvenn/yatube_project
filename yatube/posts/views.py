18a3e265bbd90fa099326bc64589ca59e76fc99b
Olvenn <olga-vencel@yandex.ru>
2023-01-30 18:46:26 +0300

    Create a basic template and templates for the header and footer.

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -5,10 +5,13 @@ from django.http import HttpResponse
 # Create your views here.
 # Главная страница
 def index(request):
-    return HttpResponse('Главная страница')
+    template = 'posts/index.html'
+    return render(request, template)
 
 
 # Страница со списком мороженого
 def group_posts(request, slug):
-    return HttpResponse(('Cтраница, на которой будут посты,'
-                         f'для группы под названием {slug}.'))
+    template = 'posts/group_list.html'
+    return render(request, template)
+    # return HttpResponse(('Cтраница, на которой будут посты,'
+    #                      f'для группы под названием {slug}.'))
